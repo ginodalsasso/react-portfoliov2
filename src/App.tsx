d4f897a8996fedc9a1cfd5642b2ec1f52ff658d4
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2025-07-25 20:08:15 +0200

    UPDATE: Move Logo component and styles to a new directory structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Logo from './components/layout/ui/Logo'
+import Logo from './components/layout/ui/Logo/Logo'
 
 function App() {
 
--- src/components/layout/ui/Logo/Logo.tsx
+++ src/components/layout/ui/Logo/Logo.tsx
@@ -1,8 +1,9 @@
+import type { JSX } from "react";
 import styles from "./Logo.module.css";
 import type { LogoProps } from "./Logo.types";
 
 export default function Logo({ size = 64, href, ariaLabel = "Portfolio Logo" }: LogoProps) {
-    const Wrapper = href ? 'a' : 'div';
+    const Wrapper = href ? 'a' : 'div' as keyof JSX.IntrinsicElements;
     return (
         <Wrapper
             className={styles.logoSquare}
