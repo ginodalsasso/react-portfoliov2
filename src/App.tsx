c46c6bdba8f0528225fc8cdc631e52b40f1442fa
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2025-07-27 19:06:06 +0200

    UPDATE: - add font sizes variables. - trying another type of animation on button and adding style to it.

--- src/App.tsx
+++ src/App.tsx
@@ -4,33 +4,58 @@ import Navbar from "./components/layout/Navbar/Navbar";
 import Button from "./components/layout/Button/Button";
 import { gsap } from "gsap";
 import { TextPlugin } from "gsap/dist/TextPlugin";
-import { SplitText } from "gsap/all";
+import { SplitText } from "gsap/dist/SplitText";
+import { useEffect } from "react";
+
 
 function App() {
-    // Initialize GSAP TextPlugin
-    gsap.registerPlugin(TextPlugin);
-    // split elements with the class "split" into words and characters
-    SplitText.create(".split", {
-        type: "lines, words",
-        mask: "lines",
-        autoSplit: true,
-        onSplit(self) {
-            return gsap.from(self.words, {
-                duration: 1,
-                y: 100,
-                autoAlpha: 0,
-                ease: "power4.out",
-                stagger: 0.05,
-            });
-        },
-    });
+    gsap.registerPlugin(TextPlugin, SplitText);
+    useEffect(() => {
+        const split = new SplitText(".split", {
+            type: "chars",
+        });
+
+        const animation = gsap.fromTo(
+            split.chars,
+            { opacity: 0 },
+            {
+                opacity: 1,
+                ease: "none",
+                duration: 0.05,
+                stagger: 0.08,
+                repeat: -1,
+                yoyo: true,
+                repeatDelay: 1,
+            }
+        );
+
+        const hoverButton = document.querySelector(".split");
+        if (hoverButton) {
+            hoverButton.addEventListener("mouseenter", () => animation.pause());
+            hoverButton.addEventListener("mouseleave", () => animation.resume());
+        }
+
+        return () => {
+            split.revert(); // Clean up SplitText instance
+            animation.kill(); // Kill the animation
+            if (hoverButton) { // Remove event listeners in order to prevent memory leaks
+                hoverButton.removeEventListener("mouseenter", () => animation.pause());
+                hoverButton.removeEventListener("mouseleave", () => animation.resume());
+            }
+        };
+    }, []);
 
     return (
         <>
             <BrowserRouter>
                 <Navbar />
-                <Button aria-label="Click me" className="split" variant="secondary" size="medium">
-                    click me
+                <Button
+                    aria-label="Click me"
+                    className="split"
+                    variant="primary"
+                    size="medium"
+                >
+                    gino.dalsasso@gmail.com
                 </Button>
             </BrowserRouter>
         </>
