af5ca932e02826624f1b74c64d8df45867da85c8
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2025-10-13 13:21:15 +0200

    UPDATE: Merge vitest.config and vite.config files. NEW: vitest/ui package.

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
@@ -13,5 +13,16 @@ export default defineConfig({
         strictPort: true,
         host: true,
         // origin: "http://0.0.0.0:8080", // for Docker compatibility
+    },
+    test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: "./src/test/setup.ts",
+        css: true,
+        coverage: {
+            provider: "v8",
+            reporter: ["text", "json", "html"],
+            reportsDirectory: "./coverage",
+        },
     },
 });
