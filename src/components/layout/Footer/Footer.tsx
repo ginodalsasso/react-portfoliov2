2abcc1ff2f9a46035a4d36f45c5d577827b36d05
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2026-02-01 14:30:36 +1300

    NEW: pictogram image to public directory and last CV

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -8,17 +8,39 @@ export default function Footer() {
     const footerRef = useLayeredAnimation(); // use of layered pin animation hook
 
     return (
-        <footer className={`${styles.footer} layered-animation`} ref={footerRef}>
+        <footer
+            className={`${styles.footer} layered-animation`}
+            ref={footerRef}
+        >
             <div className="centered-section-logo">
                 <Logo size={40} bgColor="white" roundColor="black" />
             </div>
             <nav aria-label="Footer Navigation" className={styles.nav}>
                 <FullLogo className={styles.fullLogo} />
                 <ul className={styles.navLinks}>
-                    <li><a target="_blank" rel="noopener noreferrer" href="https://github.com/ginodalsasso">[ github ]</a></li>
-                    <li><a target="_blank" rel="noopener noreferrer" href="https://linkedin.com/in/gino-dalsasso">[ linkedin ]</a></li>
                     <li>
-                        <a href="/assets/cv.pdf" target="_blank" rel="noopener noreferrer">
+                        <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href="https://github.com/ginodalsasso"
+                        >
+                            [ github ]
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href="https://linkedin.com/in/gino-dalsasso"
+                        >
+                            [ linkedin ]
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="/assets/cv-gino-dalsasso.pdf"
+                            download="cv-gino-dalsasso"
+                        >
                             <Button
                                 aria-label="Download my CV"
                                 variant="secondary"
@@ -32,7 +54,11 @@ export default function Footer() {
             </nav>
             <div className={styles.footerInfo}>
                 &copy; 2025, designed by&nbsp;
-                <a href="https://www.studiotuttopasta.com/" target="_blank" rel="noopener noreferrer">
+                <a
+                    href="https://www.studiotuttopasta.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                     Romane
                 </a>
             </div>
